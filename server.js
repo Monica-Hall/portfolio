f2d617ace4ae997231558534bef258c1cb4a6f15
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2020-06-04 10:41:09 -0700

    updated server

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express(); 
 // const {SERVER_PORT} = process.env;
 
-app.use(express.static(path.join(__dirname + '/index.html')));
+app.use(express.static(path.join(__dirname, '/index.html')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname + '/index.html'))
 });
 
 app.listen(4444, () => console.log(`Running on port 4444`)) 
